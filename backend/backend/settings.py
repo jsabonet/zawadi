7be8872567291edbe1a5fff83bc4791f1f83faf8
Joel <jsabonete09@gmail.com>
2025-07-18 23:29:54 +0200

    m

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-24bz+)rcp=q)i7a-!_@26l_e-!ss07t$qxv(84e!rbld*!jgln
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['zawadi-1.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['zawadi-1.onrender.com', 'localhost', '127.0.0.1', 'wwww.zawdidigital.com']
 
 # Application definition
 
